76f3c521ce903508cf015eeb76379710321e10fc
Egor Prokopov <egorprokopov216@gmail.com>
2024-01-28 18:35:30 +0300

    moved vitmae training loop to training.py module

--- src/vitmae/dataset.py
+++ src/vitmae/dataset.py
@@ -1,7 +1,7 @@
 import os
 
 from PIL import Image
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 
 
 class BubblesDataset(Dataset):
@@ -19,3 +19,47 @@ class BubblesDataset(Dataset):
         inputs = self.image_processor(images=image, return_tensors="pt")
 
         return inputs
+
+
+def init_datasets(
+        train_images_dir,
+        val_images_dir,
+        image_processor
+):
+    train_dataset = BubblesDataset(
+        images_dir=train_images_dir,
+        image_processor=image_processor
+    )
+    val_dataset = BubblesDataset(
+        images_dir=val_images_dir,
+        image_processor=image_processor
+    )
+
+    return train_dataset, val_dataset
+
+
+def init_dataloaders(
+        train_dataset,
+        val_dataset,
+        batch_size_train=64,
+        batch_size_val=32,
+        pin_memory=True,
+        num_workers=4
+):
+    train_dataloader = DataLoader(
+        train_dataset,
+        batch_size=batch_size_train,
+        shuffle=True,
+        pin_memory=pin_memory,
+        num_workers=num_workers
+    )
+
+    val_dataloader = DataLoader(
+        val_dataset,
+        batch_size=batch_size_val,
+        shuffle=False,
+        pin_memory=pin_memory,
+        num_workers=num_workers
+    )
+
+    return train_dataloader, val_dataloader
