7ee5ce90c861e7a95e70c23edaf1b31c9f9c70bf
Egor Prokopov <egorprokopov216@gmail.com>
2024-01-29 20:22:17 +0300

    SAM+YOLO segmentation with narrowing

--- src/sam_segmentation/utils.py
+++ src/sam_segmentation/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 
 
 def unite_masks(masks):
@@ -37,3 +38,41 @@ def unite_masks(masks):
         combined_mask = np.logical_or(combined_mask, mask)
 
     return combined_mask.astype(np.uint8) * 255
+
+
+def masks_narrowing(masks, narrowing=0.1):
+    """
+    Narrows the provided binary masks by applying distance transformation and thresholding.
+    Args:
+        masks (list of numpy.ndarray): A list of binary masks where each element represents
+            a segmented region (1 for foreground, 0 for background).
+        narrowing (float, optional): The factor by which the masks should be narrowed.
+            Defaults to 0.1, representing 10% narrowing.
+
+    Returns:
+        list of numpy.ndarray: A list of narrowed binary masks.
+
+    Notes:
+        The input masks should have the same dimensions.
+        The narrowing factor determines the threshold applied to the distance-transformed masks.
+
+    Example:
+        mask1 = np.array([[0, 1, 0],
+                          [1, 1, 0],
+                          [0, 0, 1]])
+
+        mask2 = np.array([[1, 0, 1],
+                          [0, 1, 1],
+                          [1, 0, 0]])
+
+        narrowed_masks = masks_narrowing([mask1, mask2], narrowing=0.2)
+        # Output: List of narrowed masks based on the 20% narrowing factor.
+    """
+    masks_narrowed = []
+    for mask in masks:
+        _, thresh = cv2.threshold(mask, 127, 255, 0)
+        dist_expanded = cv2.distanceTransform(mask, cv2.DIST_L2, 5)
+        _, mask_narrowed = cv2.threshold(dist_expanded, narrowing * dist_expanded.max(), 255, cv2.THRESH_BINARY)
+        masks_narrowed.append(mask_narrowed)
+
+    return masks_narrowed
