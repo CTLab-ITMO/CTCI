84d31d0b7ac709a88f95854191d3792c46f3f017
Egor Prokopov <egorprokopov216@gmail.com>
2024-02-28 23:09:21 +0300

    edit transformers_dataset

--- src/features/segmentation/transformers_dataset.py
+++ src/features/segmentation/transformers_dataset.py
@@ -7,20 +7,25 @@ from torch.utils.data import Dataset
 
 
 class SegmentationDataset(Dataset):
+    def __len__(self):
+        return len(self.images_list)
+
     def __init__(
             self,
             images_dir: str,
             masks_dir: str,
-            # image_transform=None,
-            # mask_transform=None,
+            image_transform=None,
+            mask_transform=None,
+            augmentation_transform=None,
             image_processor=None
     ):
         super().__init__()
 
         self.images_dir = images_dir
         self.masks_dir = masks_dir
-        # self.image_transform = image_transform
-        # self.mask_transform = mask_transform
+        self.image_transform = image_transform
+        self.mask_transform = mask_transform
+        self.augmentation_transform = augmentation_transform
         self.image_processor = image_processor
 
         self.images_list = os.listdir(self.images_dir)
@@ -28,21 +33,24 @@ class SegmentationDataset(Dataset):
 
         assert len(self.images_list) == len(self.masks_list), "some images or masks are missing"
 
-    def __len__(self):
-        return len(self.images_list)
-
     def _read_image_and_mask(self, image_name):
         image = cv2.imread(osp.join(self.images_dir, image_name))
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         mask = cv2.imread(osp.join(self.masks_dir, image_name), cv2.IMREAD_GRAYSCALE)
 
         image = torch.from_numpy(image)
-        mask = torch.from_numpy(mask) / 255  # TODO: make normalization
+        mask = torch.from_numpy(mask) / 255
 
         return image, mask
 
     def __getitem__(self, idx):
         image, mask = self._read_image_and_mask(self.images_list[idx])
+
+        if self.augmentation_transform:
+            seed = torch.random.seed()
+            torch.manual_seed(seed)
+            image = self.augmentation_transform(image)
+            mask = self.augmentation_transform(image)
 
         encoded_inputs = self.image_processor(image, mask, return_tensors="pt")
         for k, v in encoded_inputs.items():
