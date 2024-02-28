6bfbd723a9443de5c502c609968481e46643b2a3
Egor Prokopov <egorprokopov216@gmail.com>
2024-02-28 23:10:04 +0300

    add TransformersTrainer

--- src/models/train.py
+++ src/models/train.py
@@ -2,52 +2,102 @@ import torch
 import numpy as np
 from tqdm import tqdm
 from torch.optim import Adam
+import torch.nn as nn
 
 
-class Trainer():
-    def __init__(self,
-                 model,
-                 device,
-                 epoch_num,
-                 optim_params: dict) -> None:
+class Trainer:
+    def __init__(
+            self,
+            model,
+            optimizer,
+            device="cpu"
+    ) -> None:
 
         self.model = model
-        self.optim = Adam(model.parameters(), **optim_params)
-        self.epoch_num = epoch_num
+        self.optim = optimizer
         self.device = device
 
-    def train(self, train_dataloader, val_dataloader):
-        train_epoch_loss = []
-        val_epoch_loss = []
-        for _ in range(self.epoch_num):
+        self.history = {"train": [], "val": []}
 
-            train_batch_loss = []
-            val_batch_loss = []
+    def _train_epoch(self, train_dataloader):
+        train_batch_loss = []
+        self.model = self.model.to(self.device)
+        self.model.train()
+        for input, target in tqdm(train_dataloader):
+            input = input.to(self.device)
+            target = target.to(self.device)
 
-            # think of training strategy
-            # add logger for loss
-            self.model.to(self.device)
-            self.model.train()
-            for input, target in tqdm(train_dataloader):
-                input = input.to(self.device)
-                target = target.to(self.device)
+            self.optim.zero_grad()
+            loss_train = self.model.train_on_batch(input, target)
+            train_batch_loss.append(loss_train)
+            loss_train.backward()
+            self.optim.step()
 
-                self.optim.zero_grad()
-                loss_train = self.model.train_on_batch(input, target)
-                train_batch_loss.append(loss_train)
-                loss_train.backward()
-                self.optim.step()
+        return train_batch_loss
 
-            self.model.eval()
-            for input, target in tqdm(val_dataloader):
-                input = input.to(self.device)
-                target = target.to(self.device)
+    def _val_epoch(self, val_dataloader):
+        val_batch_loss = []
 
-                loss_val = self.model.train_on_batch(input, target)
-                val_batch_loss.append(loss_val)
+        self.model = self.model.to(self.device)
+        self.model.eval()
+        for input, target in tqdm(val_dataloader):
+            input = input.to(self.device)
+            target = target.to(self.device)
+
+            loss_val = self.model.train_on_batch(input, target)
+            val_batch_loss.append(loss_val)
+
+        return val_batch_loss
+
+    def train(self, train_dataloader, val_dataloader, epoch_num=5):
+        self.history = {"train": [], "val": []}
+        for epoch in range(epoch_num):
+            print(f"Epoch: {epoch}")
+            train_batch_loss = self._train_epoch(train_dataloader)
+            val_batch_loss = self._val_epoch(val_dataloader)
             
-            train_epoch_loss.append(np.mean(train_epoch_loss))
-            val_epoch_loss.append(np.mean(val_epoch_loss))
+            self.history["train"].append(np.mean(train_batch_loss))
+            self.history["val"].append(np.mean(val_batch_loss))
 
-        return train_epoch_loss, val_epoch_loss
+        return self.history
 
+
+class TransformerTrainer(Trainer):
+    def __init__(self, model, optimizer, device="cpu"):
+        super().__init__(model, optimizer, device)
+
+    def _train_epoch(self, train_dataloader):
+        train_batch_loss = []
+        self.model = self.model.to(self.device)
+        self.model.train()
+        for batch in tqdm(train_dataloader):
+            pixel_values = batch["pixel_values"].to(self.device)
+            labels = batch["labels"].to(self.device)
+
+            self.optim.zero_grad()
+
+            outputs = self.model(pixel_values=pixel_values, labels=labels)
+            loss, logits = outputs.loss, outputs.logits
+
+            loss.backward()
+            self.optim.step()
+
+            train_batch_loss.append(loss.item())
+
+        return train_batch_loss
+
+    def _val_epoch(self, val_dataloader):
+        val_batch_loss = []
+        metric_batch = []
+        self.model = self.model.to(self.device)
+        self.model.eval()
+        for batch in tqdm(val_dataloader):
+            pixel_values = batch["pixel_values"].to(self.device)
+            labels = batch["labels"].to(self.device)
+
+            outputs = self.model(pixel_values=pixel_values, labels=labels)
+            loss, logits = outputs.loss, outputs.logits
+
+            val_batch_loss.append(loss.item())
+
+        return val_batch_loss
