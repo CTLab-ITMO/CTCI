0cf63665ccfb055d7afaec8ad26dfb692d074d53
EgorProkopov <egorprokopov216@gmail.com>
2024-03-19 00:02:15 +0300

    create reproducibility file with set seed function

---
+++ src/models/utils/reproducibility.py
@@ -0,0 +1,12 @@
+import random
+import torch
+import numpy as np
+
+
+def set_seed(seed=0):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+
