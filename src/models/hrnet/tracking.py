c4c847f41eedfb341670d20b1fde5f72a671485b
EgorProkopov <egorprokopov216@gmail.com>
2024-03-20 22:22:01 +0300

    create training_config and tracking for hrnet

---
+++ src/models/hrnet/tracking.py
@@ -0,0 +1,39 @@
+import sys
+import os.path as osp
+
+import transformers
+
+from src.features.segmentation.dataset import SegmentationDataset
+from src.models.hrnet.model import HRNet
+from src.models.tracking import tracking_experiment
+from src.models.utils.config import read_yaml_config
+
+
+if __name__ == "__main__":
+    config_path = sys.argv[1]
+    config_data = read_yaml_config(config_path)
+
+    model_name = config_data['model']['model_name']
+    model = HRNet(freeze_backbone=False)
+
+    # TODO: create a func to init datasets from config_data
+    train_dataset = SegmentationDataset(
+        images_dir=osp.join(config_data['dataset']['training_dataset_dirs'][0], "images"),
+        masks_dir=osp.join(config_data['dataset']['training_dataset_dirs'][0], "masks")
+    )
+    val_dataset = SegmentationDataset(
+        images_dir=osp.join(config_data['dataset']['validation_dataset_dirs'][0], "images"),
+        masks_dir=osp.join(config_data['dataset']['validation_dataset_dirs'][0], "masks")
+    )
+
+    experiment_name = config_data['mlflow']['experiment_name']
+    if experiment_name == "None":
+        experiment_name = model_name
+
+    tracking_experiment(
+        model,
+        train_dataset, val_dataset,
+        config_data,
+        experiment_name=experiment_name
+    )
+
