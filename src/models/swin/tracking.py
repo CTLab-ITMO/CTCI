27526ceb8fca227b43a74fd3fba6bed4e7f4b5bf
EgorProkopov <egorprokopov216@gmail.com>
2024-03-20 21:31:24 +0300

    edit swin for testing and experiment running

--- src/models/swin/tracking.py
+++ src/models/swin/tracking.py
@@ -4,7 +4,7 @@ sys.path.append('..')
 
 from transformers import AutoImageProcessor, Swinv2Model
 
-from src.features.segmentation.transformers_dataset import SegmentationDataset
+from src.features.segmentation.dataset import SegmentationDataset
 from src.models.swin.model import Swin
 from src.models.tracking import tracking_experiment
 from src.models.utils.config import read_yaml_config
@@ -13,7 +13,10 @@ if __name__ == "__main__":
     config_path = sys.argv[1]
     config_data = read_yaml_config(config_path)
 
-    model_str = rf"microsoft/{config_data['model']['model_name']}-{config_data['model']['model_type']}"
+    model_name = config_data['model']['model_name']
+    model_type = config_data['model']['model_type']
+
+    model_str = rf"microsoft/{model_name}-{model_type}"
     image_processor = AutoImageProcessor.from_pretrained(model_str)
     net = Swinv2Model.from_pretrained(model_str)
 
@@ -32,5 +35,5 @@ if __name__ == "__main__":
         model,
         train_dataset, val_dataset,
         config_data,
-        experiment_name="swin-test"
+        experiment_name=model_name
     )
