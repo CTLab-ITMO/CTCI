8e47b0dfe853a70ba9076fd3901e6d36397b6270
Daria <43551406+sidsnow@users.noreply.github.com>
2024-04-15 16:40:31 +0300

    add check key?

--- src/models/utils/config.py
+++ src/models/utils/config.py
@@ -5,13 +5,18 @@ class ConfigHandler:
     def __init__(self, config_data):
         self.config_data = config_data
 
-    def read(self, *keys):
+    def check_key(self, *keys):
         data = self.config_data
         for key in keys:
             try:
                 data = data[key]
             except KeyError:
                 print(f"Cannot find key '{key}' in config file")
+                return None
+        return data
+
+    def read(self, keys):
+        data = self.check_key(*keys)
         return data
 
 
