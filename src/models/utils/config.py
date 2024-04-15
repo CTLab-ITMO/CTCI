f43ea1743480fe15eb9336e71e98bc41493129bf
EgorProkopov <egorprokopov216@gmail.com>
2024-04-15 11:31:32 +0300

    add config_handler

--- src/models/utils/config.py
+++ src/models/utils/config.py
@@ -1,8 +1,24 @@
 import yaml
 
 
+class ConfigHandler:
+    def __init__(self, config_data):
+        self.config_data = config_data
+
+    def read(self, *keys):
+        data = self.config_data
+        for key in keys:
+            try:
+                data = data[key]
+            except KeyError:
+                print(f"Cannot find key '{key}' in config file")
+        return data
+
+
 def read_yaml_config(file_path):
     with open(file_path, 'r') as file:
-        config = yaml.safe_load(file)
-    return config
+        config_data = yaml.safe_load(file)
+
+    config_handler = ConfigHandler(config_data)
+    return config_handler
 
