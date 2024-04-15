99e9e030c848fb512e80364dd3de85e93a96b919
EgorProkopov <egorprokopov216@gmail.com>
2024-04-15 13:37:11 +0300

    edit config reading in tracking.py

--- src/infrastructure/tracking.py
+++ src/infrastructure/tracking.py
@@ -10,6 +10,7 @@ import torch.optim.lr_scheduler as lr_scheduler
 from src.models.utils.dirs import check_dir, create_folder, create_run_folder
 from src.models.utils.reproducibility import set_seed
 from src.models.utils.models_settings import set_image_processor_to_datasets, set_gpu
+from src.models.utils.config import ConfigHandler
 from src.models.metrics import Recall, Precision, Accuracy, DiceMetric, IoUMetric
 from src.models.train import Trainer
 from src.visualization.visualization import draw_results, draw_history
@@ -18,29 +19,29 @@ from src.visualization.visualization import draw_results, draw_history
 def tracking_run(
         trainer,
         train_dataloader, val_dataloader, adele_dataloader,
-        config_data, run_name=None
+        config_handler: ConfigHandler, run_name=None
 ):
-    random_seed = config_data['random_seed']
+    random_seed = config_handler.read('random_seed')
 
-    train_batch_size = config_data['dataloader']['train_batch_size']
-    val_batch_size = config_data['dataloader']['val_batch_size']
+    train_batch_size = config_handler.read('dataloader', 'train_batch_size')
+    val_batch_size = config_handler.read('dataloader', 'val_batch_size')
 
-    model_name = config_data['model']['model_name']
-    model_type = config_data['model']['model_type']
-    model_save_dir = config_data['model']['save_dir']
+    model_name = config_handler.read('model', 'model_name')
+    model_type = config_handler.read('model', 'model_type')
+    model_save_dir = config_handler.read('model', 'save_dir')
 
-    optimizer_name = config_data['optimizer']['name']
-    optimizer_lr = config_data['optimizer']['lr']
+    optimizer_name = config_handler.read('optimizer', 'name')
+    optimizer_lr = config_handler.read('optimizer', 'lr')
 
-    adele = config_data['training']['adele']
-    epoch_num = config_data['training']['epoch_num']
+    adele = config_handler.read('training', 'adele')
+    epoch_num = config_handler.read('training', 'epoch_num')
 
-    draw_plots = config_data['history']['draw_plots']
-    draw_result = config_data['history']['draw_result']
-    plots_line_width = config_data['history']['plots_line_width']
-    plots_fontsize = config_data['history']['plots_fontsize']
-    plots_figure_name = config_data['history']['plots_figure_name']
-    results_figure_name = config_data['history']['results_figure_name']
+    draw_plots = config_handler.read('history', 'draw_plots')
+    draw_result = config_handler.read('history', 'draw_result')
+    plots_line_width = config_handler.read('history', 'plots_line_width')
+    plots_fontsize = config_handler.read('history', 'plots_fontsize')
+    plots_figure_name = config_handler.read('history', 'plots_figure_name')
+    results_figure_name = config_handler.read('history', 'results_figure_name')
 
     if run_name == 'None':
         run_name = f"{model_name}-{model_type}"
@@ -67,14 +68,12 @@ def tracking_run(
         mlflow.log_param('lr', optimizer_lr)
         mlflow.log_param('random_seed', random_seed)
 
-        # TODO: Log losses and metrics from all training epoch to DataFrame
         report_df = pd.DataFrame(metrics_num)
         report_df['train_loss'] = history['train']
         report_df['val_loss'] = history['val']
         report_df.to_csv(osp.join(model_save_dir, f"report.csv"))
         mlflow.log_artifact(osp.join(model_save_dir, f"report.csv"))
 
-        # TODO: Log loss as metrics
         mlflow.log_metric('train_loss', history['train'][-1])
         mlflow.log_metric('val_loss', history['val'][-1])
 
@@ -101,23 +100,23 @@ def tracking_run(
 def tracking_experiment(
         model,
         train_dataset, val_dataset,
-        config_data,
+        config_handler: ConfigHandler,
         scheduler=None,
         adele_dataset=None,
         experiment_name="experiment"
 ):
-    random_seed = config_data['random_seed']
+    random_seed = config_handler.read('random_seed')
     set_seed(random_seed)
-    adele_dataloader=None
+    adele_dataloader = None
 
-    image_size = config_data['dataset']['image_size']
+    image_size = config_handler.read('dataset', 'image_size')
 
-    train_batch_size = config_data['dataloader']['train_batch_size']
-    val_batch_size = config_data['dataloader']['val_batch_size']
-    pin_memory = config_data['dataloader']['pin_memory']
-    num_workers = config_data['dataloader']['num_workers']
+    train_batch_size = config_handler.read('dataloader', 'train_batch_size')
+    val_batch_size = config_handler.read('dataloader', 'val_batch_size')
+    pin_memory = config_handler.read('dataloader', 'pin_memory')
+    num_workers = config_handler.read('dataloader', 'num_workers')
 
-    device_name = config_data['model']['device']
+    device_name = config_handler.read('model', 'device')
 
     set_image_processor_to_datasets(model, image_size, [train_dataset, val_dataset])
     train_dataloader = DataLoader(
@@ -137,9 +136,9 @@ def tracking_experiment(
     device = set_gpu(device_name)
     model.device = device
 
-    optimizer_lr = config_data['optimizer']['lr']
-    betas = config_data['optimizer']['betas']
-    weight_decay = config_data['optimizer']['weight_decay']
+    optimizer_lr = config_handler.read('optimizer', 'lr')
+    betas = config_handler.read('optimizer', 'betas')
+    weight_decay = config_handler.read('optimizer', 'weight_decay')
 
     optimizer = torch.optim.AdamW(model.parameters(), lr=optimizer_lr, betas=betas, weight_decay=weight_decay)
 
@@ -160,11 +159,11 @@ def tracking_experiment(
         "precision": Precision().to(device),
         "recall": Recall().to(device)
     }
-    main_metric_name = config_data["training"]["main_metric"]
+    main_metric_name = config_handler.read("training", "main_metric")
     if main_metric_name not in [str(metric) for metric in metrics]:
         main_metric_name = "iou"
 
-    model_save_dir = config_data['model']['save_dir']
+    model_save_dir = config_handler.read('model', 'save_dir')
     if model_save_dir == 'None':
         directory = f".\\checkpoints\\{experiment_name}"
         if not check_dir(directory):
@@ -182,10 +181,10 @@ def tracking_experiment(
     )
 
     mlflow.set_experiment(experiment_name=experiment_name)
-    config_data["mlflow"]["experiment_name"] = experiment_name
+    config_handler.config_data["mlflow"]["experiment_name"] = experiment_name
     tracking_run(
         trainer=trainer,
         train_dataloader=train_dataloader, val_dataloader=val_dataloader,
         adele_dataloader=adele_dataloader,
-        config_data=config_data
+        config_handler=config_handler
     )
