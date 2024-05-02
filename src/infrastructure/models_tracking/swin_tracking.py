796b4985211582b1b00ffa0505ad7641315f2833
Daria <obobojk@gmail.com>
2024-05-02 09:43:36 +0300

    add default looking tracking file and config

--- src/infrastructure/models_tracking/swin_tracking.py
+++ src/infrastructure/models_tracking/swin_tracking.py
@@ -15,49 +15,50 @@ Args:
 
 import sys
 
-import albumentations as albu
-from transformers import Swinv2Model
+sys.path.append('../../models')
 
 from src.features.segmentation.dataset import get_train_dataset_by_config, get_val_dataset_by_config
-from src.models.swin.model import Swin
+from src.models.swin.model import build_swin
 from src.infrastructure.tracking import tracking_experiment
+from src.models.utils.augmentation import get_augmentations_from_config, get_preprocess_from_config
 from src.models.utils.config import read_yaml_config
-
-sys.path.append('../../models')
 
 
 if __name__ == "__main__":
     config_path = sys.argv[1]
     config_handler = read_yaml_config(config_path)
 
-    model_name = config_handler.read('model', 'model_name')
-    model_type = config_handler.read('model', 'model_type')
+    model = build_swin(config_handler)
 
-    model_str = rf"microsoft/{model_name}-{model_type}"
-    net = Swinv2Model.from_pretrained(model_str)
+    tr = get_preprocess_from_config(config_handler)
+    aug = get_augmentations_from_config(config_handler)
 
-    model = Swin(net=net)
+    train_dataset = get_train_dataset_by_config(
+        config_handler,
+        transform=tr,
+        augmentation_transform=aug
+    )
+    val_dataset = get_val_dataset_by_config(
+        config_handler,
+        transform=tr,
+        augmentation_transform=aug
+    )
+    adele_dataset = get_train_dataset_by_config(
+        config_handler,
+        transform=tr,
+        augmentation_transform=None
+    )
 
-    transform = albu.Compose([
-        albu.Resize(config_handler.read('dataset', 'image_size', 'height'), config_handler.read('dataset', 'image_size', 'width')),
-        albu.CLAHE(always_apply=True),
-        albu.Normalize(always_apply=True),
-        albu.RandomCrop(config_handler.read('dataset', 'image_size', 'height'),
-                        config_handler.read('dataset', 'image_size', 'width'), p=0.4),
-        albu.MaskDropout(max_objects=10),
-        albu.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05, rotate_limit=30, p=0.5)
-    ])
-
-    train_dataset = get_train_dataset_by_config(config_handler, transform)
-    val_dataset = get_val_dataset_by_config(config_handler, transform)
+    adele_dataset.return_names = True
 
     experiment_name = config_handler.read('mlflow', 'experiment_name')
     if experiment_name == "None":
-        experiment_name = model_name
+        experiment_name = "swinv2"
 
     tracking_experiment(
         model,
         train_dataset, val_dataset,
         config_handler,
-        experiment_name=experiment_name
+        experiment_name=experiment_name,
+        adele_dataset=adele_dataset
     )
