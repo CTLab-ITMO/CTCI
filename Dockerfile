31cbc50634565dbf6dee1973bd7b54823a5c4648
Daria <43551406+sidsnow@users.noreply.github.com>
2024-09-27 14:29:27 +0300

    move config to hydra, add lightning and init model with hydra

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,6 @@ COPY ./setup.py /app
 RUN apt-get update
 RUN pip install -r requirements.txt
 
-
 LABEL authors="PC"
 
 ENTRYPOINT ["top", "-b"]
---
+++ src/config.py
@@ -0,0 +1,61 @@
+import typing as tp
+
+from omegaconf import OmegaConf, DictConfig
+from pydantic import BaseModel, ConfigDict
+
+
+class _BaseValidatedConfig(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True, extra='forbid')
+
+
+class LossConfig(_BaseValidatedConfig):
+    name: str
+    weight: float
+    loss_fn: str
+    loss_kwargs: dict
+
+
+class AugmentationConfig(_BaseValidatedConfig):
+    train: tp.Optional[DictConfig] = None
+    valid: tp.Optional[DictConfig] = None
+
+
+class DataConfig(_BaseValidatedConfig):
+    img_size: tp.List[int] = [224, 224]
+    batch_size: int = 32
+    num_workers: int = 6
+    pin_memory: bool = True
+    num_samples: int = 1024
+
+
+class ModuleConfig(_BaseValidatedConfig):
+    model: tp.Dict[str, tp.Any]  # type: ignore
+    threshold: float
+
+    lr: float
+    optimizer: str
+    scheduler: str
+
+    cls_losses: tp.List[LossConfig]
+    seg_losses: tp.List[LossConfig]
+
+
+class TrainerConfig(_BaseValidatedConfig):
+    fast_dev_run: bool
+    accelerator: str
+    log_every_n_steps: int
+    max_epochs: int
+    min_epochs: int
+
+
+class ExperimentConfig(_BaseValidatedConfig):
+    project_name: str
+    experiment_name: str
+
+    data_config: DataConfig
+    trainer_config: TrainerConfig
+
+    @classmethod
+    def from_yaml(cls, path: str) -> 'ExperimentConfig':
+        cfg = OmegaConf.to_container(OmegaConf.load(path), resolve=True)
+        return cls(**cfg)
