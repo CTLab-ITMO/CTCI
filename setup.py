a1a109cdd95ebf194bf8733c0ef36db53ec6ead9
Daria <43551406+sidsnow@users.noreply.github.com>
2024-10-05 20:29:20 +0300

    add lightning and hydra to requirements, change makefile, add dockerfile

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Clumped texture composite images projects (froath, rocks and others texture images)',
+    description='Clumped texture composite images projects (froth, rocks and others texture images)',
     author='s70c3',
     license='MIT',
 )
