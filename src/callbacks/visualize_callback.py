3e898866188c1dd0b522d9743227d60113fd42de
Daria <obobojk@gmail.com>
2024-11-14 17:26:20 +0300

    working ver

--- src/callbacks/visualize_callback.py
+++ src/callbacks/visualize_callback.py
@@ -15,24 +15,16 @@ class VisualizationCallback(pl.Callback):
         self.alpha = alpha
 
     def on_validation_epoch_end(self, trainer, pl_module):
-        logger = trainer.logger
-        if not hasattr(logger, "experiment") or not logger.experiment:
-            print("ClearML logger not found or not initialized.")
-            return
-
-        # Get a batch of validation data
         val_dataloader = trainer.datamodule.val_dataloader()
         val_batch = next(iter(val_dataloader))
         images = val_batch[0]
         images = images.to(pl_module.device)
 
-        # Predict masks
         with torch.no_grad():
             predictions = pl_module(images)
             predictions = torch.sigmoid(predictions)
             predictions = predictions > 0.5
 
-        # Visualize and log results
         fig, axes = plt.subplots(self.max_images, 3, figsize=(15, 5 * self.max_images))
         for i in range(min(self.max_images, len(images))):
             image = images[i].cpu().permute(1, 2, 0).numpy()
@@ -44,7 +36,7 @@ class VisualizationCallback(pl.Callback):
             axes[i, 0].imshow(image)
             axes[i, 0].set_title("Original Image")
             axes[i, 0].axis("off")
-            axes[i, 1].imshow(pred_mask, cmap="Blues")
+            axes[i, 1].imshow(pred_mask.squeeze(), cmap="Blues")
             axes[i, 1].set_title("Prediction")
             axes[i, 1].axis("off")
             axes[i, 2].imshow(overlay)
@@ -52,14 +44,7 @@ class VisualizationCallback(pl.Callback):
             axes[i, 2].axis("off")
 
         plt.tight_layout()
-
-        # Log figure to ClearML
-        logger.experiment.report_matplotlib_figure(
-            title="Segmentation Results",
-            series="Validation Results",
-            figure=fig,
-            iteration=trainer.current_epoch,
-        )
+        trainer.logger.experiment.add_figure("Validation Results", fig, trainer.current_epoch)
         plt.close(fig)
 
     def _overlay_mask(self, image, mask):
